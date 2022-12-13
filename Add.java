4cd56d90eeb4125e2b13f7968c38519dd138bae6
sana sayyed <117528278+sayyedsanaakib@users.noreply.github.com>
2022-12-13 20:33:09 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+public class Add {
+	public static void main(String[] args) {
+		int n1;
+		int n2;
+		int r;
+		
+		n1 = 10;
+		n2 = 20;
+		
+		r = n1 + n2;
+		
+		System.out.println("Sum = " + r);
+	}
+}
